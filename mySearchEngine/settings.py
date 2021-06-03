8a25c1107b8e115af33058d7c87219354a44ae63
Pranjal <pranjalsingh1600@gmail.com>
2021-06-03 17:12:02 +0530

    modified files in template folder, django app working fine

--- mySearchEngine/settings.py
+++ mySearchEngine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mySearchEngine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],#telling to go to templates folder to get html page
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/Users/Acer/Desktop/Django Search Engine/mySearchEngine/static/',
+]
 
 STATIC_URL = '/static/'
 
