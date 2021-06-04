5f58db58f750b56461eda76d87d801b898f099b4
Pranjal <pranjalsingh1600@gmail.com>
2021-06-04 15:19:09 +0530

    search engine working now, shifted to ask.com

--- searchapp/views.py
+++ searchapp/views.py
@@ -7,27 +7,28 @@ def index(request):
     return render(request,'index.html')
 
 def search(request):
-    if request.method=='POST':
-        search=request.POST['search']
-        url='https://duckduckgo.com/?q='+search
-        #fetching url from duckduckgo.com
-        res=requests.get(url)
-        #beutifying the text of html
-        soup=bs(res.text,'lxml')
-        #found the class of the div tag that shows results
-        result_listings = soup.find_all('div',{'class':'result results_links_deep highlight_d result--url-above-snippet'})
-        #
+    if request.method == 'POST':
+        search = request.POST['search']
+        url = 'https://www.ask.com/web?q='+search
+        res = requests.get(url)
+        soup = bs(res.text, 'lxml')
+
+        result_listings = soup.find_all('div', {'class': 'PartialSearchResults-item'})
+
         final_result = []
 
         for result in result_listings:
-            result_title = result.find(class_ = 'result__title js-result-title').text
+            result_title = result.find(class_='PartialSearchResults-item-title').text
             result_url = result.find('a').get('href')
-            result_description = result.find(class_ = 'result__snippet js-result-snippet').text
-            final_result.append((result_title,result_url,result_description))
-            
+            result_desc = result.find(class_='PartialSearchResults-item-abstract').text
+
+            final_result.append((result_title, result_url, result_desc))
+
         context = {
             'final_result': final_result
         }
-        return render(request,'search.html',context)
-    else :
-        return render(request,'search.html')
+
+        return render(request, 'search.html', context)
+
+    else:
+        return render(request, 'search.html')
